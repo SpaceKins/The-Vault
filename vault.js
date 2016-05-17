435949a31ddf45a05945fa4b8570be395ca8aef6
Steven <spacekins@zoho.com>
2016-05-16 23:51:01 -1000

    added tested vault

--- vault.js
+++ vault.js
@@ -1,4 +1,18 @@
 'use strict';
-module.exports = function() {
-  
+module.exports = (function() {
+
+	var vaultObject={};
+
+	vaultObject['getValue']=function(key)  {
+		
+		return (vaultObject[key])?vaultObject[key]:null;
+	};
+
+
+	vaultObject['setValue']=function(key,value)  {
+	vaultObject[key]=value;
 };
+
+return vaultObject;
+
+});
